e2bc121aa2e9525bc56afe2793384c93ece47fcd
Eduard Schumacher <107512513+mijamoto911@users.noreply.github.com>
2025-01-29 16:51:33 +0100

    add avatar

--- main.py
+++ main.py
@@ -5,9 +5,22 @@ from src.api import contacts, utils, auth, users
 from slowapi.errors import RateLimitExceeded
 from slowapi.util import get_remote_address
 from starlette.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(utils.router, prefix="/api")
 app.include_router(contacts.router, prefix="/api")
 app.include_router(auth.router, prefix="/api")
